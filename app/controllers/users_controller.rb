b3adf13d85ef854ecbca2539ecd99e01d1ff5597
Dietrich <dietricherwane@yahoo.fr>
2016-01-11 07:57:49 +0000

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,12 @@ class UsersController < ApplicationController
   def select_sign_up_profile
     @registration_active = "active"
   end
+
+  def has_rib
+    certified_agent_id = params[:certified_agent_id]
+    status = ""
+    (User.where(certified_agent_id: certified_agent_id, sub_certified_agent_id: nil).first.rib rescue nil).blank? ? status = "0" : status = "1"
+
+    render text: status
+  end
 end
