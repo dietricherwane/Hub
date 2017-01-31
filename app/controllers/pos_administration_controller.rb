29d911e857216e2423d75020dc74bd1c9e2b5ca4
Dietrich <dietricherwane@yahoo.fr>
2017-01-31 16:50:49 +0000

    changes

--- app/controllers/pos_administration_controller.rb
+++ app/controllers/pos_administration_controller.rb
@@ -1,6 +1,8 @@
 class PosAdministrationController < ApplicationController
   before_filter :sign_out_disabled_users
 
+  layout :layout_used
+
   def list_agents
     @agents = User.where("certified_agent_id IS NOT NULL")
   end
